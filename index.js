d318c959acf31b5f0d1239f5185f8a7b493cde0d
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-09-07 21:07:59 -0400

    builds get route for hobbits

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ const express = require('express');
 const server = express();
 server.get('/', (req, res) => { //request handler function and "/" is our root
     res.send('hello world!')
+});
+
+server.get('/hobbits', (req, res) => {
+    const hobbits = [
+        {
+            id: 1,
+            name: 'Samwise Gangee'
+        },
+        {
+            id: 2,
+            name: 'frodo Baggins'
+        }
+    ];
+
+    res.status(200).send(hobbits);
 })
 
 server.listen(8000, () => console.log('API running on port 8000'));
