9f45fd05a1d69bf832c33c66694c4e3c2a68a034
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-09-08 10:39:38 -0400

    POST, PUT and DELETE adds

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const express = require('express');
 
 const server = express();
+server.use(express.json())
+
 server.get('/', (req, res) => { //request handler function and "/" is our root
     res.send('hello world!')
 });
@@ -19,6 +21,18 @@ server.get('/hobbits', (req, res) => {
     ];
 
     res.status(200).send(hobbits);
+})
+
+server.post('/hobbits', (req, res) => {
+    res.status(201).json({url: '/hobbits', operation: 'POST'})
+}) //CREATE data
+
+server.put('/hobbits', (req, res) => {
+    res.status(200).json({url: '/hobbits', operation: 'PUT'})
+}) //UPDATE
+
+server.delete('/hobbits', (req, res) => {
+    res.status(204)
 })
 
 server.listen(8000, () => console.log('API running on port 8000'));
