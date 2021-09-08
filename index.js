100e3d080644ebae1f98f3fd54f407391b5758d1
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-09-07 20:45:42 -0400

    express installs, server creates

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // implement your API here
+const express = require('express');
+
+const server = express();
+server.get('/', (req, res) => { //request handler function and "/" is our root
+    res.send('hello world!')
+})
+
+server.listen(8000, () => console.log('API running on port 8000'));
